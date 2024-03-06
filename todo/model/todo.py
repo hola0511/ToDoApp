1818b16d9501450dfcc4d155a79ee0eb841066a2
hola0511 <espinosaariassamuel@gmail.com>
2024-03-06 11:21:52 -0500

    coco del codigo terminado

--- todo/model/todo.py
+++ todo/model/todo.py
@@ -14,11 +14,24 @@ class Todo:
         if tag not in self.tags:
             self.tags.append(tag)
         else:
-            print("el elemento ya esta en la lista")
+            print("El elemento ya esta en la lista")
 
     def __str__(self):
         return print(self.code_id, " - ", self.title)
 
 
 class TodoBook:
-    pass
+
+    def __init__(self):
+        self.todos: dict[int, Todo] = []
+
+    def add_todo(self, title: str, description: str) -> int:
+        pass
+
+    def pending_todos(self) -> list[Todo]:
+        pass
+
+    def completed_todos(self) -> list[Todo]:
+        pass
+
+    def tags_todo_count(self) -> dict[str,int]:
